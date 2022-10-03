2237211c753cec7c20a889ca544747576fe8c71c
gonzallito <goncaloferreira2408@gmail.com>
2022-10-03 20:21:05 +0100

    Started aula 3

---
+++ 3/aula_3.hs
@@ -0,0 +1,23 @@
+
+-- 3.1 -
+
+primeiro f p x = map f (filter p x)
+
+
+
+-- 3.2 -
+
+dec2int :: [Int] -> Int
+
+dec2int = foldl (\res n -> 10 * res + n) 0
+
+
+
+-- 3.3 -
+
+zipWith1 :: (a -> b -> c) -> [a] -> [b] -> [c]
+
+zipWith1 f [] _ = []
+zipWith1 f _ [] = []
+zipWith1 f (x:xs) (y:ys) = (f x y):(zipWith1 f xs ys)
+-- zipWith1 f x y = [f a b | (a,b) <- zip x y]
