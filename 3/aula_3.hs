8cc80bb13bb36e3cb520b0e2b34529a8909b754c
gonzallito <goncaloferreira2408@gmail.com>
2022-10-03 20:46:07 +0100

    aula 3

--- 3/aula_3.hs
+++ 3/aula_3.hs
@@ -1,4 +1,6 @@
 
+import Data.List
+
 -- 3.1 -
 
 primeiro f p x = map f (filter p x)
@@ -21,3 +23,12 @@ zipWith1 f [] _ = []
 zipWith1 f _ [] = []
 zipWith1 f (x:xs) (y:ys) = (f x y):(zipWith1 f xs ys)
 -- zipWith1 f x y = [f a b | (a,b) <- zip x y]
+
+
+
+-- 3.4 -
+
+isort :: Ord a => [a] -> [a]
+
+isort x = foldr insert [] x
+-- isort = foldr insert []
