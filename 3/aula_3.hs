df2970e3a7cd6de8118892910e2fabebdc6c2837
gonzallito <goncaloferreira2408@gmail.com>
2022-10-08 15:50:47 +0100

    Done until 3.8

--- 3/aula_3.hs
+++ 3/aula_3.hs
@@ -32,3 +32,75 @@ isort :: Ord a => [a] -> [a]
 
 isort x = foldr insert [] x
 -- isort = foldr insert []
+
+
+
+-- 3.5 -
+
+-- a )
+
+maximum1 :: Ord a => [a] -> a
+
+maximum1 = foldl1 max
+-- tambem dá foldr1
+
+minimum1 :: Ord a => [a] -> a
+
+minimum1 = foldr1 min
+-- tambem dá foldl1
+
+
+
+-- b )
+
+myFoldl1 f x = foldl f (head x) (tail x)
+
+myFoldr1 f x = foldr f (last x) (init x)
+
+
+
+-- 3.6 -
+
+mdc a b = fst (until (\(a,b) -> b == 0) (\(a,b) -> (b,a`mod`b)) (a,b))
+
+
+
+-- 3.7 -
+
+-- a )
+
+(+++) :: [a] -> [a] -> [a]
+
+x +++ y = foldr (:) y x
+
+
+
+-- b )
+
+concatFoldr :: [[a]] -> [a]
+
+concatFoldr = foldr (++) []
+
+
+
+-- c )
+
+reverseFoldr :: [a] -> [a]
+
+reverseFoldr = foldr (\x y -> y ++ [x]) []
+
+
+
+-- d )
+
+reverseFoldl :: [a] -> [a]
+
+reverseFoldl = foldl (\x y -> y:x) []
+
+
+
+-- e )
+
+elemAny :: Eq a => a -> [a] -> Bool
+
+elemAny x y = any (x==) y
